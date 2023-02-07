07dad1f7c2db921305bd12aec9dd118b0074a29f
Mohamed-Echbiy <echbiymed@gmail.com>
2023-02-07 20:55:19 +0100

    config cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 //
 app.use(
   cors({
-    origin: ["https://animo-timev3.vercel.app/", "http://localhost:3000"],
+    origin: ["https://animo-timev3.vercel.app", "http://localhost:3000"],
   })
 );
 app.use(express.json());
